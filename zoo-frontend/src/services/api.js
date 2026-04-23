81e3090ded6183bbbea2127304522488556d20ba
rehema <mukarubibirehema@gmail.com>
2026-04-24 00:31:20 +0300

    Set production API URL

--- zoo-frontend/src/services/api.js
+++ zoo-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: 'https://zoo-management-service.onrender.com/api',
 });
 
 // Add a request interceptor to add the JWT token to the headers
